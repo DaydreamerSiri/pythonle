479a8ff6a5eb015303551cfd63ca6a83b0cdf114
Akihiro <33292042+akihiroAwa@users.noreply.github.com>
2018-08-03 16:39:51 +0200

    experimental branch 0.61 update
    die welt lädt nun
    - Spiel ist noch nicht spielbar

--- src/game.py
+++ src/game.py
@@ -7,36 +7,39 @@ class Game(object):
     def __init__(self):
         self.listcreatures = Creatures()
         self.rand = random
+        self.check = True
     def startgame(self):
         while True:
             "Input Game #1"
-            a = 0
+            self.a = 0
             rawq = raw_input("\nhi")
             if rawq == "hi" or rawq == "hallo":
                 rawq = raw_input("\nwie geht es ihnen?")
                 if rawq == "gut":
                     print("\ngleich aber nicht mehr")
-                    while True:
+                    while self.check == True:
                         time.sleep(1)
-                        a += 1
-                        print("Loading")
-                        if a == 10:
-                            b = 0
-                            rawq = raw_input("\nselbstzerstoerungseinheit wird"
-                            "aktiviert zum abbrechen bitte die 1 druecken")
-                            while True:
-                                b += 1
-                                time.sleep(1.1)
-                                print(b)
-                                if b == 10:
-                                    print("sie haben verloren programm schliesst "
-                                    "sich in 10 sekunden")
-                                    time.sleep(10)
-                                    quit()
+                        self.a += 1
+                        print("Loading") 
+                        if self.a == 10:
+                            self.check = False
+                    else:
+                        b = 0
+                        rawq = raw_input("\nselbstzerstoerungseinheit wird"
+                        "aktiviert zum abbrechen bitte die 1 druecken")
+                        while True:
+                            if b == 10:
+                                print("sie haben verloren programm schliesst "
+                                "sich in 10 sekunden")
+                                time.sleep(10)
+                                quit()
                             if rawq == "1":
                                 print("\nhmm sie haben doch noch glueck gehabt"
                                 "!")
                                 self.world1()
+                            b += 1
+                            time.sleep(1.1)
+                            print(b)
             time.sleep(20)
         return
     def checkworld(self):
@@ -53,7 +56,8 @@ class Game(object):
             print("\nLade Welt bitte warten...")
             if a == 10:
                 break
-        self.listcreatures.player(100, 60, 20)
+        player = self.listcreatures.Player()
+        player.player()
         print("\nDu siehst einen Pfad voraus")
         print("\nwas willst du tun?")
         print("moegliche Optionen:" 
@@ -63,8 +67,7 @@ class Game(object):
             print("\n moegliche Optionen:\n forward, back,left,right")
             self.playerinput = raw_input("")
             if self.playerinput == "forward":
-                self.checkworld()
-                
+                self.checkworld()  
                 
                 
             
